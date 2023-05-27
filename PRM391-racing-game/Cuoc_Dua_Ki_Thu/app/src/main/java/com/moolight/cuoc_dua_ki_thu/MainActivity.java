cf028b17ac14dc299c49ebec571ed4a13b048326
moo-light <binhnguyenthanh19242yahoo@gmail.com>
2023-05-27 22:37:17 +0700

    .

--- PRM391-racing-game/Cuoc_Dua_Ki_Thu/app/src/main/java/com/moolight/cuoc_dua_ki_thu/MainActivity.java
+++ PRM391-racing-game/Cuoc_Dua_Ki_Thu/app/src/main/java/com/moolight/cuoc_dua_ki_thu/MainActivity.java
@@ -3,12 +3,18 @@ package com.moolight.cuoc_dua_ki_thu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+
+import com.google.android.material.slider.Slider;
 
 public class MainActivity extends AppCompatActivity {
-
+    SeekBar racer1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        racer1 = (SeekBar) findViewById(R.id.sRacer1);
+
+        racer1.setThumb(getDrawable(R.drawable.round_button));
     }
 }
